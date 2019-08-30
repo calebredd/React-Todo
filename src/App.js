918797fb06850d65a67f2f75722d435978a948a1
Caleb Redd <caleb.redd@yahoo.com>
2019-08-30 15:47:28 -0700

    To do list props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { task: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.list = [
+      { task: "Organize Garage", id: 1528817077286, completed: false }
+    ];
+    this.count = 1;
+  }
+  handleChange(event) {
+    this.setState({ task: event.target.value });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.list.push({
+      task: this.state.task,
+      id: this.count,
+      completed: false
+    });
+    event.target.value = "";
+    // console.log(this.state, this.count, this.list);
+    this.count++;
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,6 +32,19 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        {this.list.map(todo => (
+          <p key={todo.id}>{todo.task}</p>
+        ))}
+        {/* <p>{this.state.task}</p> */}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="input"
+            name="item"
+            placeholder="New Todo"
+            onChange={this.handleChange}
+          ></input>
+          <button type="submit">Add to List</button>
+        </form>
       </div>
     );
   }
